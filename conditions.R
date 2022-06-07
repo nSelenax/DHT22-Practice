b00a8703f78c8bccd389abbce3b0ad0dc6e28115
Ning Xu <ningxu@Nings-Air.ht.home>
2022-06-07 18:20:03 -0400

    conditions2

--- conditions.R
+++ conditions.R
@@ -1,1 +1,17 @@
 # snippets for if conditions
+
+userage <- 15
+
+#if the user is younger than 21, tell them to drink lemonade
+#else offer them beer
+#above is my PLAN
+
+#start
+#if the user is younger than 21, tell them to drink lemonade
+if (userage < 21) {
+  print("Here's your lemonade")
+#else offer them beer
+} else {
+  print ("Here's your beer!")
+}
+
