2cf665ff83c337e52017b8a40a72fa3b34aae28f
Ning Xu <ningxu@Nings-Air.ht.home>
2022-06-07 18:33:20 -0400

    conditions3

--- conditions.R
+++ conditions.R
@@ -15,3 +15,23 @@ if (userage < 21) {
   print ("Here's your beer!")
 }
 
+speed <- 50 
+
+# if speed < 40, you are SLOW
+if (speed < 40) {
+  print("slow")
+} else if (speed <= 70) {
+  print("residential")
+} else if (speed <= 120) {
+  print("highway")
+} else {
+  print("ticket")
+}
+
+#do ascending or descending order, don't exchange 70 and 120, otherwise it would exist after fulfilling < 120
+# if speed is between 40-70, you are @Residential
+# if speed > 120 you get a ticket
+# if speed between 70-120 then highway
+
+
+
